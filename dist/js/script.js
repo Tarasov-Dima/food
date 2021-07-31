3033a1da6953f08e992467a08378896e1c2e8e0b
Dima <dmytrotarasov9@gmail.com>
2021-07-31 19:50:11 +0200

    created function for change tabs with animation

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,33 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener('DOMContentLoaded', () => {
+  let menuBlock = document.querySelector('.tabheader__items'),
+      menu = document.querySelectorAll('.tabheader__item'),
+      tabs = document.querySelectorAll('.tabcontent'); // Tabs
 
+  menuBlock.addEventListener('click', event => {
+    let link = event.target;
+
+    if (link && link.classList.contains("tabheader__item")) {
+      menu.forEach((menuLink, number) => {
+        if (menuLink == link) {
+          removeClass(menu);
+          removeClass(tabs);
+          link.classList.add('tabheader__item_active', 'fade');
+          tabs[number].classList.add('tabcontent_active', 'fade');
+        }
+      });
+    }
+  });
+
+  function removeClass(arr) {
+    arr.forEach(item => {
+      item.classList.remove('tabcontent_active', 'fade');
+      item.classList.remove('tabheader__item_active', 'fade');
+    });
+  }
+});
 
 /***/ })
 
